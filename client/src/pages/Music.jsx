04fceeb2c08201a39345ca87762ec14ba6299b47
C.RANJITH KUMAR <83603627+CodingRanjith@users.noreply.github.com>
2024-06-07 11:09:54 +0530

    Update Music.jsx

--- client/src/pages/Music.jsx
+++ client/src/pages/Music.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  max-width: 100%;
+  max-width: 800px;
   margin: 0 auto;
   padding: 20px;
   text-align: center;
