8e7a7a728f6aa43bc24eb72f61dd1b8e0a8d1161
C.RANJITH KUMAR <83603627+CodingRanjith@users.noreply.github.com>
2024-06-07 11:08:04 +0530

    Update AddWorkout.jsx

--- client/src/components/AddWorkout.jsx
+++ client/src/components/AddWorkout.jsx
@@ -26,7 +26,31 @@ const Title = styled.div`
   }
 `;
 
-const AddWorkout = ({ workout, setWorkout, addNewWorkout, buttonLoading }) => {
+const SampleWorkout = styled.div`
+  cursor: pointer;
+  color: ${({ theme }) => theme.secondary};
+`;
+
+const AddWorkout = ({ setWorkout, addNewWorkout, buttonLoading }) => {
+  const sampleWorkouts = [
+    "#Legs\n-Back Squat\n-5 setsX15 reps\n-30 kg\n-10 min",
+    "#Arms\n-Bicep Curls\n-3 setsX12 reps\n-15 kg\n-8 min",
+    "#Core\n-Plank\n-3 sets\n-1 min each\n-No weight",
+    "#Chest\n-Bench Press\n-4 setsX10 reps\n-50 kg\n-12 min",
+    "#Back\n-Deadlift\n-4 setsX8 reps\n-70 kg\n-15 min",
+    "#Shoulders\n-Shoulder Press\n-3 setsX12 reps\n-20 kg\n-10 min",
+    "#Cardio\n-Running\n-30 minutes\n-6 km/h\n-30 min",
+    "#Stretching\n-Yoga\n-30 minutes\n-No weight\n-30 min",
+    "#HIIT\n-Burpees\n-5 setsX20 reps\n-No weight\n-20 min",
+    "#Full Body\n-Circuit Training\n-4 setsX10 reps\n-Bodyweight\n-30 min",
+    "#Glutes\n-Squats\n-4 setsX12 reps\n-40 kg\n-15 min",
+    "#Triceps\n-Tricep Dips\n-3 setsX10 reps\n-Bodyweight\n-8 min",
+    "#Hamstrings\n-Deadlifts\n-4 setsX8 reps\n-60 kg\n-12 min",
+    "#Calves\n-Calf Raises\n-3 setsX15 reps\n-Bodyweight\n-6 min",
+    "#Upper Body\n-Push-Ups\n-4 setsX12 reps\n-Bodyweight\n-10 min",
+  ];
+  
+
   return (
     <Card>
       <Title>Add New Workout</Title>
@@ -42,9 +66,15 @@ const AddWorkout = ({ workout, setWorkout, addNewWorkout, buttonLoading }) => {
 -Reps
 -Weight
 -Duration`}
-        value={workout}
         handelChange={(e) => setWorkout(e.target.value)}
       />
+      <div>
+        {sampleWorkouts.map((workout, index) => (
+          <SampleWorkout key={index} onClick={() => setWorkout(workout)}>
+            {workout}
+          </SampleWorkout>
+        ))}
+      </div>
       <Button
         text="Add Workout"
         small
